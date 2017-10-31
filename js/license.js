6b6d95a7f07869a9c6e1306fc600fa3d13bacfa7
Stewart Allen <stewart@neuron.com>
2017-10-30 20:52:39 -0400

    bump rev

--- js/license.js
+++ js/license.js
@@ -1,7 +1,7 @@
 var exports = {
     COPYRIGHT:"Copyright (C) 2014-2017 Stewart Allen <stewart@neuron.com> - All Rights Reserved",
     LICENSE:"Unauthorized copying, modification and re-distribution are strictly prohibited without prior written consent.",
-    VERSION:"1.0.66"
+    VERSION:"1.0.67"
 };
 if (!module) var module = {};
 module.exports = exports;
