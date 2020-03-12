99482e1006889155fe6ed254c57f1d3dbeacfdf4
Stewart Allen <stewart.allen@gmail.com>
2020-03-11 22:11:07 -0400

    fix help api call. bump rev for next release

--- js/license.js
+++ js/license.js
@@ -1,7 +1,7 @@
 var exports = {
     COPYRIGHT:"Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
     LICENSE:"See the license.md file included with the source distribution",
-    VERSION:"1.7.3"
+    VERSION:"1.7.4"
 };
 if (!module) var module = {};
 module.exports = exports;
