301e6bef769494fd145bec3f8a2ef32050a9b3c5
Stewart Allen <stewart.allen@gmail.com>
2020-07-03 18:38:49 -0400

    bump rev v2.1.4

--- src/license.js
+++ src/license.js
@@ -3,7 +3,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: "2.1.3"
+        VERSION: "2.1.4"
     };
 
     if (typeof(module) === 'object') {
