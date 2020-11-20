6f022b066909ccafd30210a81b31038d19662064
Stewart Allen <stewart.allen@gmail.com>
2020-11-20 13:13:14 -0500

    release rev, disable beta label

--- src/kiri/init.js
+++ src/kiri/init.js
@@ -45,7 +45,7 @@
         o2js = API.o2js,
         platform = API.platform,
         selection = API.selection,
-        beta = 2311;
+        beta = 0;
 
     let deviceLock = false,
         selectedTool = null,
