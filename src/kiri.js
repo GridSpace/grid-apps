c764cd9aaf5a7cd01c718088fd9cf5ca959045ed
Stewart Allen <stewart.allen@gmail.com>
2020-12-13 23:58:35 -0500

    bump rev 2.5.D3

--- src/kiri.js
+++ src/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         self.kiri = {
-            beta: 2502,
+            beta: 2503,
             driver: {}, // driver modules
             loader: []  // module loading: array of functions
         };
