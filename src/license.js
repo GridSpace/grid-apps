0dfdedb7d816c991e79f52f199fe2675b2472d9a
Stewart Allen <stewart@neuron.com>
2020-12-20 11:16:20 -0500

    bump rev 2.4.4

--- src/license.js
+++ src/license.js
@@ -3,7 +3,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: "2.4.3"
+        VERSION: "2.4.4"
     };
 
     if (typeof(module) === 'object') {
