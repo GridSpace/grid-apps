3d4e9e8733c36c16b6efb14f7f11fca7df21a56f
Stewart Allen <stewart@neuron.com>
2021-01-16 17:42:44 -0500

    bump rev 2.6.D6

--- src/kiri.js
+++ src/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         self.kiri = {
-            beta: 2605,
+            beta: 2606,
             driver: {}, // driver modules
             loader: []  // module loading: array of functions
         };
