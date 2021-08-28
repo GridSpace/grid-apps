cc861291a4a0b83016b847c670c3c35df9880f31
Stewart Allen <sa@grid.space>
2021-08-27 22:14:57 -0400

    bump rev 3.0.D17

--- src/kiri.js
+++ src/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         self.kiri = {
-            beta: 3016,
+            beta: 3017,
             driver: {}, // driver modules
             loader: []  // module loading: array of functions
         };
