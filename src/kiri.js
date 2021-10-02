4ceea31750e4bbc44e6b32b12d66a6711adf8dfc
Stewart Allen <stewart.allen@gmail.com>
2021-10-01 20:59:57 -0400

    bump rev 3.0.D27

--- src/kiri.js
+++ src/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         self.kiri = {
-            beta: 3026,
+            beta: 3027,
             driver: {}, // driver modules
             loader: []  // module loading: array of functions
         };
--- src/license.js
+++ src/license.js
@@ -3,7 +3,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: "3.0.D26"
+        VERSION: "3.0.D27"
     };
 
     if (typeof(module) === 'object') {
