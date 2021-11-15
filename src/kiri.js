92312e50b054dbccdd7f9e4de04ba82b9dffeba4
Stewart Allen <stewart.allen@gmail.com>
2021-11-15 14:10:42 -0500

    bump rev 3.1.D1

--- src/kiri.js
+++ src/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         let kiri = self.kiri = {
-            beta: 3100,
+            beta: 3101,
             driver: {}, // driver modules
             loader: [], // module loading: array of functions
             load: (fn) => kiri.loader.push(fn)
