faf891d353297b59fe1a62ceef28531fab372967
Stewart Allen <stewart.allen@gmail.com>
2022-01-21 11:51:38 -0500

    begin 3.2 dev cycle

--- src/main/kiri.js
+++ src/main/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         let kiri = self.kiri = {
-            beta: 3120,
+            beta: 3200,
             driver: {}, // driver modules
             loader: [], // module loading: array of functions
             load: (fn) => kiri.loader.push(fn)
