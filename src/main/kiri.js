c9c09fa782eae12dd9debfbd7a33e0f1ab4f17d0
Stewart Allen <sa@grid.space>
2022-02-12 14:45:57 -0500

    start 3.3.0 dev cycle

--- src/main/kiri.js
+++ src/main/kiri.js
@@ -6,7 +6,7 @@
 
     if (!self.kiri) {
         let kiri = self.kiri = {
-            beta: 3214,
+            beta: 3300,
             driver: {}, // driver modules
             loader: [], // module loading: array of functions
             load: (fn) => kiri.loader.push(fn)
