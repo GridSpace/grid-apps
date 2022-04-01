5cd272ca44a400fa20e3a6af17c364296a1790a1
Stewart Allen <stewart.allen@gmail.com>
2022-04-01 16:15:30 -0400

    bless 3.3.0

--- src/moto/license.js
+++ src/moto/license.js
@@ -3,7 +3,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: "3.3.D16"
+        VERSION: "3.3.0"
     };
 
     if (typeof(module) === 'object') {
