18c82ecde785734c0df44891f880de86076702a5
Stewart Allen <sa@grid.space>
2022-04-06 19:32:48 -0700

    bump rev 3.4.D2

--- src/main/kiri.js
+++ src/main/kiri.js
@@ -15,7 +15,7 @@ gapp.main("main.kiri", [], (root) => {
     const modfns = root.kirimod = ( root.kirimod || [] );
 
     const kiri = root.kiri = {
-        beta: 3401,
+        beta: 3402,
         driver: {}, // driver modules
         load(fn) {
             modfns.push(fn);
