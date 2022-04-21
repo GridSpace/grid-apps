1127105421293ebe19a8ac31c44cb4188346ea22
Stewart Allen <stewart.allen@gmail.com>
2022-04-20 20:27:29 -0400

    bump rev 3.4.D5

--- src/main/kiri.js
+++ src/main/kiri.js
@@ -15,7 +15,7 @@ gapp.main("main.kiri", [], (root) => {
     const modfns = root.kirimod = ( root.kirimod || [] );
 
     const kiri = root.kiri = {
-        beta: 3404,
+        beta: 3405,
         driver: {}, // driver modules
         load(fn) {
             modfns.push(fn);
