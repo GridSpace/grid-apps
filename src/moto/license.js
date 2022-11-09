0b320bc1e1cb3d21a748e85656ee7d9562deeba9
Stewart Allen <stewart.allen@gmail.com>
2022-11-08 20:27:20 -0500

    bump rev 3.7.D9

--- src/moto/license.js
+++ src/moto/license.js
@@ -5,7 +5,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: (is_self ? self : this).debug_version || "3.7.D8"
+        VERSION: (is_self ? self : this).debug_version || "3.7.D9"
     };
 
     if (typeof(module) === 'object') {
