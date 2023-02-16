c4f2df1942600d2f2db49f8c8750a99c7c807059
Stewart Allen <stewart.allen@gmail.com>
2023-02-15 19:52:56 -0500

    bump rev 3.9.D4

--- src/main/kiri.js
+++ src/main/kiri.js
@@ -15,7 +15,7 @@ gapp.main("main.kiri", [], (root) => {
     const modfns = root.kirimod = ( root.kirimod || [] );
 
     const kiri = root.kiri = {
-        beta: 3903,
+        beta: 3904,
         driver: {}, // driver modules
         load(fn) {
             modfns.push(fn);
--- src/moto/license.js
+++ src/moto/license.js
@@ -5,7 +5,7 @@
     let terms = {
         COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
         LICENSE: "See the license.md file included with the source distribution",
-        VERSION: (is_self ? self : this).debug_version || "3.9.D3"
+        VERSION: (is_self ? self : this).debug_version || "3.9.D4"
     };
 
     if (typeof(module) === 'object') {
