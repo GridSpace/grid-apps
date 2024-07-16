cdb4cdd0a914e8f45073581b91c0f89a8d42a791
Stewart Allen <sa@grid.space>
2024-07-16 10:45:28 -0600

    add gerber inferences for boundaries on borderless cu which triggers a z raise for open traces

--- src/load/gbr.js
+++ src/load/gbr.js
@@ -16,8 +16,9 @@ load.GBR = {
     toMesh
 };
 
-function toMesh(text, opt = { z: 1 }) {
+function toMesh(text, opt = {}) {
     const progress = opt.progress || function(){};
+    const debug = opt.debug ? console.log : function(){};
 
     // for now ignore open trace lines
     // future exports should allow a way to represent them for cnc
@@ -29,13 +30,33 @@ function toMesh(text, opt = { z: 1 }) {
     // next level down should be the remaining copper trace which we
     // subtract from the board to produce paths that can be milled
     const nest = POLYS.nest([...closed, ...circs, ...rects], true);
+    debug({ nest });
+
+    if (nest.length === 0) return [].toFloat32();
 
     const flat = [];
     for (let n of nest) n.flattenTo(flat, [], true);
     progress(8);
+    debug({ flat });
 
     const z0 = opt.z  || opt.z0 || 1;
     const z1 = opt.z1 || 0.25;
+
+    // encapsulate all polys in one enclosing rect with an offset
+    if (nest.length > 1) {
+        const offset = (opt.offset || 2) * 2;
+        const bounds = nest[0].bounds;
+        for (let p of nest) {
+            bounds.merge(p.bounds);
+        }
+        const rect = base.newPolygon().centerRectangle(bounds.center(0), bounds.width() + offset, bounds.height() + offset);
+        for (let n of nest) {
+            n.depth++;
+        }
+        flat.push(rect);
+        debug({ bounds, flat });
+        opt.raise = z1;
+    }
 
     // board outline
     const top = [];
@@ -43,10 +64,11 @@ function toMesh(text, opt = { z: 1 }) {
         top.push(poly.extrude(z0));
     }
     progress(15);
+    debug({ top });
 
     // nothing to render
     if (top.length === 0) {
-        return [];
+        return [].toFloat32();
     }
 
     // copper pads
@@ -55,6 +77,7 @@ function toMesh(text, opt = { z: 1 }) {
         mid.push(poly.extrude(z1, z0 - z1));
     }
     progress(20);
+    debug({ mid });
 
     // if no traces, return board
     if (mid.length === 0) {
@@ -71,9 +94,10 @@ function toMesh(text, opt = { z: 1 }) {
     const opn = [];
     for (let poly of POLYS.union(open_exp, 0, true)) {
         const tool = poly.tool;
-        opn.push(poly.extrude(z0));
+        opn.push(poly.extrude(z0 + (opt.raise || 0)));
     }
     progress(25);
+    debug({ opn });
 
     // inner polys for traces which we add back to the top
     // and (optionally) remove from the bottom for making vias
