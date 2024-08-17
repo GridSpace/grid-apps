ad53e78e7eb2f80e9632a2a633cf7be524d97b10
Stewart Allen <stewart.allen@gmail.com>
2024-08-16 23:10:12 -0400

    bump rev 4.0.20, 1.5.2

--- src/main/mesh.js
+++ src/main/mesh.js
@@ -22,7 +22,7 @@ const { mesh, moto } = root;
 const { broker } = gapp;
 const { space } = moto;
 
-const version = mesh.version = '1.5.1';
+const version = mesh.version = '1.5.2';
 const call = broker.send;
 const dbindex = [ "admin", "space" ];
 
