6cf288765690af94a68bae8e919f3dde5b7c5d3d
Stewart Allen <stewart.allen@gmail.com>
2025-02-05 13:18:14 -0500

    when running in electron, change KM title to help since KM is in the window name

--- mods/electron/electron.js
+++ mods/electron/electron.js
@@ -3,6 +3,7 @@ self.kiri.load(api => {
     console.log('ELECTRON MODULE RUNNING');
     api.electron = {};
     api.event.on('init-done', () => {
+        $('app-name-text').innerText = "More Info";
         $('top-sep').style.display = 'flex';
     });
     api.stats.set('kiri', self.kiri.version + 'e');
