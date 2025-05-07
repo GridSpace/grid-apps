e740c89bad1aad4348677cb1109511da625c4b46
bakedpotatolord <jeffy9745@gmail.com>
2025-05-06 21:44:25 -0600

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  useTabs: false,
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "crlf"
+}
+
+module.exports = config
