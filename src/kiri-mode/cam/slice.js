38622b40d9f55baa587b7692d2b0d8d0c0f61c34
bakedpotatolord <jeffy9745@gmail.com>
2025-05-31 09:25:38 -0600

    fix drill bug

--- src/kiri-mode/cam/slice.js
+++ src/kiri-mode/cam/slice.js
@@ -500,6 +500,7 @@ CAM.holes = async function(settings, widget, diam) {
                 continue;
             }
             for (let poly of inner) {
+                if ( poly.points.length < 7 ) continue;
                 let center = poly.calcCircleCenter();
                 center.area = poly.area();
                 center.overlapping = [center]
