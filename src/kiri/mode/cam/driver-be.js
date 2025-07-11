c0d866b40ebdeb9d65af587bd91005d85714bba9
bakedpotatolord <jeffy9745@gmail.com>
2025-07-11 16:40:13 -0600

    fix syntax for 4.3

--- src/kiri/mode/cam/driver-be.js
+++ src/kiri/mode/cam/driver-be.js
@@ -5,6 +5,7 @@ import { cam_slice, holes, traces } from './slice.js';
 import { cam_prepare } from './prepare.js';
 import { cam_export } from './export.js';
 import { tool as mesh_tool } from '../../../mesh/tool.js';
+import { cylinder_poly_find } from './slice.js';
 
 import { init as init_2d } from './anim-2d-be.js';
 import { init as init_3d } from './anim-3d-be.js';
@@ -100,9 +101,9 @@ function init(worker) {
 
     dispatch.cam_cylinder_find = async function(data,send){
         const { id, face, settings } = data;
-        const widget = kiri.worker.cache[id];
+        const widget = worker.cache[id];
         try{
-            send.done(CAM.cylinder_poly_find(widget, face));
+            send.done(cylinder_poly_find(widget, face));
         }catch(error){
             send.done({error});
         }
