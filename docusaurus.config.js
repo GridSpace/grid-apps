cb041d5e3cff4b2b8b05051f49ac289c90604556
bakedpotatolord <jeffy9745@gmail.com>
2025-07-18 17:25:47 -0600

    add client redirects for legacy links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,79 @@
+import lunr from 'docusaurus-lunr-search'
 
-
-module.exports = {
+export default {
   url: 'https://docs.grid.space',
   title: 'grid.space docs',
   baseUrl : '/',
   favicon: 'img/gs-logo.png',
   staticDirectories: ['./docs/static/'],
   // ...
-  plugins: [require.resolve('docusaurus-lunr-search')],
+  plugins: [
+    lunr,
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            to: '/mesh-tool',
+            from: '/projects/mesh-tool',
+          },
+          {
+            to: '/kiri-moto',
+            from: "/projects/kiri-moto",
+          },
+
+          {
+            to: '/kiri-moto/interface',
+            from: "/projects/kiri-moto/interface",
+          },
+          {
+            to: '/kiri-moto/controls',
+            from: "/projects/kiri-moto/controls",
+          },
+          {
+            to: '/kiri-moto/integrations',
+            from: "/projects/kiri-moto/integrations",
+          },
+          {
+            to: '/kiri-moto/shared-profiles',
+            from: "/projects/kiri-moto/shared-profiles",
+          },
+          {
+            to: '/kiri-moto/gcode-macros',
+            from: "/projects/kiri-moto/gcode-macros",
+          },
+          {
+            to: '/kiri-moto/apis',
+            from: "/projects/kiri-moto/apis",
+          },
+          {
+            to: '/kiri-moto/engine-apis',
+            from: "/projects/kiri-moto/engine-apis",
+          },
+          {
+            to: '/kiri-moto/localization',
+            from: "/projects/kiri-moto/localization",
+          },
+          {
+            to: '/kiri-moto/octoprint',
+            from: "/projects/kiri-moto/octoprint",
+          },
+          {
+            to: '/kiri-moto/faq',
+            from: "/projects/kiri-moto/faq",
+          },
+          {
+            to: "/gridbot",
+            from: "/projects/gridbot"
+          },
+          {
+            to: "/gridbot/bom",
+            from: "/projects/gridbot/bom"
+          }
+        ],
+      },
+    ]
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
