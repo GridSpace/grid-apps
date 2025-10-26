7ee0d72b4a15c1f769e39db61900b2ea93c6b757
Stewart Allen <stewart.allen@gmail.com>
2025-10-26 19:18:21 -0400

    start 4.4.0 beta work

--- src/moto/license.js
+++ src/moto/license.js
@@ -3,9 +3,9 @@
 const terms = {
     COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
     LICENSE: "See the license.md file included with the source distribution",
-    VERSION: "4.3.4"
+    VERSION: "4.4.0"
 };
 
-export const beta = 0;
+export const beta = 4401;
 export const license = terms;
 export const version = terms.VERSION;
