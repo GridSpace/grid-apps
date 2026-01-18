fb150f64e7eaa2b50c8dfc4d456b657149b01ad5
Stewart Allen <stewart.allen@gmail.com>
2026-01-17 21:52:20 -0500

    bump rev 4.6.1

--- src/moto/license.js
+++ src/moto/license.js
@@ -3,7 +3,7 @@
 const terms = {
     COPYRIGHT: "Copyright (C) Stewart Allen <sa@grid.space> - All Rights Reserved",
     LICENSE: "See the license.md file included with the source distribution",
-    VERSION: "4.6.0"
+    VERSION: "4.6.1"
 };
 
 export const beta = 0;
